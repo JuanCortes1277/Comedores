e5c329b9a60d439815ad3d542cbe7179e56ed886
Juan David Cortes <david.cortes@onbusinessconsulting.com>
2020-04-11 17:19:05 -0500

    SImplex actual

--- BackEndComedores/Logic/LogicBL.cs
+++ BackEndComedores/Logic/LogicBL.cs
@@ -1021,14 +1021,91 @@ namespace BackEndComedores.Logic
             }
             return lstdisponibilities;
         }
-     
-    
+
+
 
 
         /*PROCESO DE ORDEN*/
         //Simplex
+        public static double simplex(double[] provider, int totalproviders)
+        {
 
-        
+            int variablesQuantity = provider.Length;
+
+            var solver = SolverContext.GetContext();
+            var model = solver.CreateModel();
+
+            //    var A = new[,]
+            //        {
+            //    { 1, 0, 0, 0, 0 },
+            //    { 0.760652602, 1, 0, 0, 0 },
+            //    { 0.373419404, 0.760537565, 1, 0, 0 },
+            //    { 0.136996731, 0.373331934, 0.760422587, 1, 0 },
+            //    { 0.040625222, 0.136953801, 0.373244464, 0.76030755, 1 }
+            //};
+            int[,] A = new int[variablesQuantity, variablesQuantity];
+            int r = 0;
+            for (int l = 0; l < variablesQuantity; l++)
+            {
+                for (r = 0; r < variablesQuantity; r++)
+                {
+                    if (l == r)
+                    {
+                        A[l, r] = 1;
+                    }
+                    else
+                    {
+                        A[l, r] = 0;
+                    }
+                }
+            }
+            double totalDouble = 1.0;
+            foreach (double op in provider)
+            {
+                totalDouble = op + totalDouble;
+
+
+
+            }
+
+            var b = provider;
+
+            //Console.WriteLine("Total: " + totalDouble);
+
+            var n = A.GetLength(1);
+            var x = new Decision[n];
+            for (var i = 0; i < n; ++i)
+                model.AddDecision(x[i] = new Decision(Domain.RealNonnegative, null));
+
+            // START NLP SECTION
+            var m = A.GetLength(0);
+            Term goal = 0.0;
+            for (var j = 0; j < m; ++j)
+            {
+                Term Ax = 0.0;
+                for (var i = 0; i < n; ++i)
+                {
+                    Ax += A[j, i] * x[i];
+                }
+
+                goal += (Model.Power(Ax + b[j], 2));
+            }
+            model.AddGoal(null, GoalKind.Minimize, goal / (totalDouble * totalproviders * 100));
+            // END NLP SECTION
+
+            var solution = solver.Solve();
+          //  Console.WriteLine("f = {0}", solution.Goals.First().ToDouble());
+            //for (var i = 0; i < n; ++i)
+            //{
+            //    Console.WriteLine("x[{0}] = {1}", i, x[i].GetDouble());
+            //}
+            return solution.Goals.First().ToDouble();
+            //   Console.WriteLine(totalsum);
+            // Console.ReadLine();
+
+        }
+
+
 
 
     }
